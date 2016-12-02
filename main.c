32c3ac91ede7fdb895bee5c01ace003da9789dd3
Luke <Lukasronsholt@gmail.com>
2016-12-02 16:24:46 +0100

    Added function to set array sizes
    Signed-off-by: Luke <Lukasronsholt@gmail.com>

--- main.c
+++ main.c
@@ -9,37 +9,35 @@
 #define MAX_TIME 6
 #define MAX_FLIGHT_MODEL 15
 
-int count_travel_types(char *type);
-
-typedef struct
-{
+typedef struct {
     char travel_type[MAX_TRAVEL];
     char flight_number[MAX_FLIGHT_NR];
     char date[MAX_DATE];
     char weekday[MAX_WEEKDAY];
     char time[MAX_TIME];
     char flight_model[MAX_FLIGHT_MODEL];
+} flight_data;
 
-}flight_data;
-
-typedef struct
-{
+typedef struct {
     flight_data *arrivals;
     flight_data *departure;
 } flight_type;
 
+int count_travel_types(char *type);
+
+void set_array_size(int arrivals, int departures, flight_type *flights);
+
 int main(void)
 {
 
-    int number_arrivals, number_departures;
+    int number_arrivals, number_departures, total_flights;
 
     number_arrivals = count_travel_types("ARR");
     number_departures = count_travel_types("DPT");
+    total_flights = number_arrivals + number_departures;
 
-    flight_type flights[number_arrivals + number_departures];
-
-    flights->arrivals = (flight_data*)calloc(number_arrivals, sizeof(flight_data));
-    flights->departure = (flight_data*)calloc(number_departures, sizeof(flight_data));
+    flight_type flights[total_flights];
+    set_array_size(number_arrivals, number_departures, flights);
 
     return 0;
 }
@@ -51,16 +49,12 @@ int count_travel_types(char *type)
     int c;
     char travel_type[MAX_TRAVEL];
 
-    while ((c = fgetc(fp)) != EOF)
-    {
-        fseek(fp,-1,SEEK_CUR);
-        fscanf(fp," %s %*s %*s %*s %*s %*s",
-                travel_type);
+    while ((c = fgetc(fp)) != EOF) {
+        fseek(fp, -1, SEEK_CUR);
+        fscanf(fp, " %s %*s %*s %*s %*s %*s", travel_type);
 
-        if (!strcmp(travel_type,type))
-        {
+        if (!strcmp(travel_type, type)) {
             count += 1;
-            /*printf("%s \n",travel_type);*/
         }
     }
 
@@ -68,3 +62,10 @@ int count_travel_types(char *type)
 
     return count;
 }
+
+void set_array_size(int arrivals, int departures, flight_type *flights)
+{
+    flights->arrivals = (flight_data *) calloc(arrivals, sizeof(flight_data));
+    flights->departure = (flight_data *) calloc(departures, sizeof(flight_data));
+}
+
