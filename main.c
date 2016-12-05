9834c353e7678d0f9b44a5025d17be1822049c60
Luke <Lukasronsholt@gmail.com>
2016-12-05 10:21:57 +0100

    Fra gruppearbejde.
    count_lines and arrays

--- main.c
+++ main.c
@@ -9,8 +9,7 @@
 #define MAX_TIME 6
 #define MAX_FLIGHT_MODEL 15
 
-typedef struct
-{
+typedef struct {
     char travel_type[MAX_TRAVEL];
     char flight_number[MAX_FLIGHT_NR];
     char date[MAX_DATE];
@@ -18,25 +17,78 @@ typedef struct
     char time[MAX_TIME];
     char flight_model[MAX_FLIGHT_MODEL];
 
-}flight_data;
+} flight_data;
+
+int count_lines();
+
+void scan_for_data(int total_flights, flight_data *flights);
+
+flight_data read_line(FILE *fp);
+
+void print_array(int length, flight_data *flights);
 
 int main(void)
 {
+    int total_flights = count_lines();
 
-    count_travel_types("ARR");
+    flight_data flights[total_flights];
+    scan_for_data(total_flights, flights);
 
-    flight_data arrivals[1];
-    flight_data departures[1];
+    printf("%d\n", total_flights);
+    print_array(total_flights, flights);
 }
 
-int count_travel_types(char *type)
+int count_lines()
 {
-    FILE *fp = fopen("Flight_information", "r");
+    FILE *fp = fopen("Flight_information.txt", "r");
+    int c, counter = 1;
 
-    if(!strcmp(type,"DPT"))
+    while ((c = fgetc(fp)) != EOF)
     {
-
+        if (c == '\n')
+            ++counter;
     }
 
     fclose(fp);
+
+    return counter;
+}
+
+void scan_for_data(int total_flights, flight_data *flights)
+{
+    FILE *fp = fopen("Flight_information.txt", "r");
+
+    for (int i = 0; i < total_flights; ++i)
+    {
+        flights[i] = read_line(fp);
+    }
+    fclose(fp);
+}
+
+flight_data read_line(FILE *fp)
+{
+    flight_data flight;
+    fscanf(fp, " %s %s %s %s %s %s",
+           flight.travel_type,
+           flight.flight_number,
+           flight.date,
+           flight.weekday,
+           flight.time,
+           flight.flight_model);
+
+    return flight;
+}
+
+void print_array(int length, flight_data *flights)
+{
+    for (int i = 0; i < length; ++i)
+    {
+        printf("%s %s %s %s %s %s\n",
+               flights[i].travel_type,
+               flights[i].flight_number,
+               flights[i].date,
+               flights[i].weekday,
+               flights[i].time,
+               flights[i].flight_model);
+    }
 }
